c8ed685a0c9d3dab14cfa944979cdbad783dfa21
alinko <alinkokomansuby@gmail.com>
2016-12-05 13:49:51 +0700

    Add files via upload

---
+++ v1.2/autorun.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#/************************************************
+#| Daijobu - lokomediacms scanner
+#| Codename  : yamete kudasai
+#| Version   : 1.2
+#| Author    : alinko (shutdown57)
+#| @github   : https://github.com/alintamvanz
+#| @twitter  : https://twitter.com/alinmansby
+#| copyright (c) 2k16 linuxcode.org 
+#*************************************************/
+
+
+php /opt/daijobu/daijobu.php
